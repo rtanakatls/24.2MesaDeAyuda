9931e6dde8bc8cf9e3758434c77d7fe265b32e4a
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-09-20 23:53:15 -0500

    Update MesaDeAyuda.cpp

--- MesaDeAyuda.cpp
+++ MesaDeAyuda.cpp
@@ -1,20 +1,91 @@
 // MesaDeAyuda.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <conio.h>
+
+
+using namespace std;
+
+void wait()
+{
+    for (int i = 0; i < 100000000; i++)
+    {
+
+    }
+}
+
+void show_character(int x,int y)
+{
+    for (int i = 1; i <= y; i++)
+    {
+		std::cout << "\n";
+	}
+    for (int i = 1; i <= x; i++)
+    {
+		std::cout << " ";
+	}
+    std::cout << "@";
+}
+
+void draw_right(int level) 
+{
+    int temporal_level = level;
+    if (level <= 1) 
+    {
+        temporal_level= 0;
+    }
+    for (int i = 1+(temporal_level* 3); i <= 50-(level*3); i++)
+    {
+		show_character(i, level*3);
+		wait();
+        system("cls");
+	}
+}
+
+void draw_left(int level)
+{
+    for (int i = 50 - (level * 3); i >= 1 + (level * 3); i--)
+    {
+        show_character(i, 20 - (level * 3));
+        wait();
+        system("cls");
+    }
+}
+
+void draw_down(int level)
+{
+    for (int i = 1 + (level * 3); i <= 20 - (level * 3); i++)
+    {
+        show_character(50 - (level * 3), i);
+        wait();
+        system("cls");
+    }
+}
+void draw_up(int level)
+{
+    for (int i = 20 - (level * 3); i >= 1 + ((1+level) * 3); i--)
+    {
+        show_character(1 + (level * 3), i);
+        wait();
+        system("cls");
+    }
+}
+
+void execute()
+{
+    for (int i = 0; i <= 3; i++)
+    {
+        draw_right(i);
+        draw_down(i);
+        draw_left(i);
+        draw_up(i);
+
+    }
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    execute();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
